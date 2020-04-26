335019039afab0d9d6d8db23c5c9a31148af776f
wo315 <2106936@qq.com>
2020-04-26 17:40:16 +0800

    review c language

---
+++ 1_C/malloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char* find_my_mood() {
+    char* my_mood = malloc(6 * sizeof(char));
+    my_mood[0] = 'h';
+    my_mood[1] = 'a';
+    my_mood[2] = 'p';
+    my_mood[3] = 'p';
+    my_mood[4] = 'y';
+    my_mood[5] = '\0';
+    return my_mood;
+}
+
+int main() {
+    char* mood = find_my_mood();
+    printf("I am currently %s\n", mood);
+    free(mood);
+    return 0;
+}
