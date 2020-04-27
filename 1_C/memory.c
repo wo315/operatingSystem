9cf0b6bdbf8ea9fcfde5bd143da6ccc67947695e
wo315 <2106936@qq.com>
2020-04-27 17:28:22 +0800

    1_C gdb valgrind

---
+++ 1_C/memory.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+** void * memset ( void * ptr, int value, size_t num );
+** 通常这个函数可以将一大段内存设置为某一个具体数值。非常快，经过系统优化。不用循环
+** void * memcpy ( void * destination, const void * source, size_t num );
+** 使用num段长度的source拷贝到destination.也是系统级别的，很快。
+*/
+
