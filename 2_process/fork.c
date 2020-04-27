7fe55adf420ba012b7ab3943669b99d50538aca7
wo315 <2106936@qq.com>
2020-04-27 18:12:28 +0800

    how to use fork wait exit methods

--- 2_process/fork.c
+++ 2_process/fork.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// pid_t fork(void); 包含在unistd.h头文件中。不要任何参数，直接创建一个新进程。
+// void exit(int status); 存在于stdlib.h头文件中。
+// pid_t wait(int* stat_loc); 存在于sys/wait.h
+
 int main() {
     printf("%s\n", "我是汪洋");
 
